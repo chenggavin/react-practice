40b6be070c9d735b3c89fa9fc50d5331ed96f139
Gavin Cheng <gavincheng01@gmail.com>
2017-12-10 15:02:48 -0500

    Created component ProjectItem

---
+++ src/Components/ProjectItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class ProjectItem extends Component {
+  render() {
+  	console.log(this.props);
+    return (
+      <div className="Projects">
+        My Projects
+
+         
+
+
+
+      </div>
+    );
+  }
+}
+
+export default ProjectItem;
